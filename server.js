e1953b425613997ce14a10e76b0179620b36f292
Matheus Nunes <matheusnun.ns@gmail.com>
2019-09-15 02:22:41 -0300

    added validations

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express'); // require importa a biblioteca framework
 const bodyParser = require('body-parser'); // importa biblioteca de parser.
-const Disciplina = require('./model/Disciplina');
 const app = express();// cria instância do express do modulo express
 
 
@@ -13,6 +12,7 @@ app.use(bodyParser.json()) // habilita midware para parsear o body do post e adi
 
 //banco de dados
 const alunos = [];
+const disciplinas = [];
 ///
 
 
@@ -34,3 +34,60 @@ app.post('/aluno', function(request, response) {
     alunos.push(novoAluno);
     response.json(alunos);
 });
+
+app.get('/disciplina', function (request, response) {
+    //Registra
+
+    return response.json({ disciplinas: disciplinas });
+});
+
+app.post('/disciplina', function(request, response) {
+    const { alunos, nome, horas, gradeHorario } = request.body;
+
+    gradeHorario = ;
+
+    const novaDisciplina = {
+        alunos: alunos._id,
+        nome: nome,
+        horas: horas,
+        gradeHorario: gradeHorario,
+        _id: new Date().getTime()
+    }
+    verificaGradeHorario(novaDisciplina);
+    verificaDuracaoAula(novaDisciplina);
+    disciplinas.push(novaDisciplina);
+    response.json(disciplinas);
+});
+
+function verificaDuracaoAula(disciplina) {
+    const horas = disciplina.horas;
+    if (horas > 4 || horas < 1)
+        console.log("Uma disciplina deve possuir entre 1 ou 4 horas de duração!");
+}
+
+function verificaGradeHorario(disciplina) {
+    const gradeHorario = disciplina.gradeHorario;
+    if (gradeHorario.legth > 3 || gradeHorario.legth < 1)
+        console.log("Disciplinas devem ser cadastradas em até quatro dias diferentes."); 
+} 
+const gradeDeHorarios = {
+    segunda:
+    {
+        1:[],
+        2:[],
+        3:[],
+        4:[],
+    },
+    terca:[],
+    quarta:[],
+    quinta:[],
+    sexta:[],
+}
+function matricula(disciplinas, alunos) {
+    const disciplina = disciplina._id;
+    const alunos = aluno._id;
+    for (int i = 0; i < disciplinas.legth; i++) {
+        if (disciplina[i] == hora)
+    }
+    disciplina = alunos;
+}
